badfe882bc7c2ff186c41fefc111d0f925e3788c
Shubhanshu Ratan Singh <35319804+shubhanshu7@users.noreply.github.com>
2023-03-21 20:15:15 +0530

    Add files via upload

---
+++ LLProblem3.java
@@ -0,0 +1,69 @@
+package DSA;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.io.*;
+/*You have been given a singly linked list of integers. Write a function to print the list in a reverse order.
+To explain it further, you need to start printing the data from the tail and move towards the head of the list, printing the head data at the end.*/
+
+public class LLProblem3 {
+
+static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    public static LinkedListNode<Integer> takeInput() throws IOException {
+        LinkedListNode<Integer> head = null, tail = null;
+
+        String[] datas = br.readLine().trim().split("\\s");
+
+        int i = 0;
+        while(i < datas.length && !datas[i].equals("-1")) {
+            int data = Integer.parseInt(datas[i]);
+            LinkedListNode<Integer> newNode = new LinkedListNode<Integer>(data);
+            if(head == null) {
+                head = newNode;
+                tail = newNode;
+            }
+            else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+            i += 1;
+        }
+
+        return head;
+    }
+    
+    public static void print(LinkedListNode<Integer> head){
+        while(head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+        
+        System.out.println();
+    }
+    
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t > 0) {
+
+            LinkedListNode<Integer> head = takeInput(); 
+            printReverse(head);
+            System.out.println();
+            
+            t -= 1;
+
+        }
+        
+    }
+	public static void printReverse(LinkedListNode<Integer> head) {
+ 
+		if(head==null) {
+			return;
+		}
+printReverse(head.next);
+System.out.print(head.data+" ");
+		
+	}
+    
+}
